076137b29bc0232e9363e7a705df646693f1c264
Ben Johnson <greypilgrim115@gmail.com>
2025-11-15 09:08:20 -0600

    Added getById API call

--- src/managers/postManager.js
+++ src/managers/postManager.js
@@ -3,3 +3,8 @@ const _apiUrl = "/api/post";
 export const getPosts = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
+
+
+export const getPostById = (id) => {
+  return fetch(_apiUrl+ `/${id}`).then((res) => res.json());
+};
