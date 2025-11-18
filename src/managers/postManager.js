6d9fceedf18b449aab817c12e4ff4c494443bbdb
mattnorthcutt <matt_northcutt@icloud.com>
2025-11-17 20:14:47 -0600

    Switching branches

--- src/managers/postManager.js
+++ src/managers/postManager.js
@@ -3,3 +3,7 @@ const _apiUrl = "/api/post";
 export const getPosts = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
+
+export const getPostsByUser = (userId) => {
+  return fetch(`${_apiUrl}/user/${userId}`).then((res) => res.json());
+}
